cafedc8fcec9d2035d58a8b2f45102bcdbd65f9c
marwenzitouni <marwenzitouni2020@gmail.com>
2022-10-05 16:38:52 +0100

    first commit

--- email.js
+++ email.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "marwenzitouni2020@gmail.com",
-    pass: "hirqynmapbpyqmsd",
+    pass: "hirqynmap*****",
   },
 });
 
