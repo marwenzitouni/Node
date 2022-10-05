9b6cb284e755c93261682dd8237d87cb9016f5b2
marwenzitouni <marwenzitouni2020@gmail.com>
2022-10-05 12:34:42 +0100

    first commit

---
+++ email.js
@@ -0,0 +1,24 @@
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "marwenzitouni2020@gmail.com",
+    pass: "hirqynmapbpyqmsd",
+  },
+});
+
+var mailOptions = {
+  from: "marwenzitouni2020@gmail.com",
+  to: "abdelhafidh.bensalem1990@gmail.com",
+  subject: "Sending Email using Node.js",
+  text: "Hello ABS",
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Email sent: " + info.response);
+  }
+});
